bd39669e8d12c2d25e5052b76f461f8bf94b6bab
guruh.nandana <guruh.nandana@gopay.co.id>
2023-11-15 16:37:23 +0700

    [Guruh] Pushing my 1st repo!

---
+++ first_source/js_test.js
@@ -0,0 +1,8 @@
+// In script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const myButton = document.getElementById('myButton');
+
+    myButton.addEventListener('click', function() {
+        alert('Guruh is HERE');
+    });
+});
